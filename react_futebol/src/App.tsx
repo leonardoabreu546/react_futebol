3dabd15829527687eebd4c1605c3299addf74826
Leonardo <lcat140507@gmail.com>
2026-03-24 22:32:35 +0000

    navegação entre páginas com usestate

--- react_futebol/src/App.tsx
+++ react_futebol/src/App.tsx
@@ -1,26 +1,36 @@
 import CardEquipa from './card_equipa'
 import CardJogador from './card_jogador'
+import { useState } from 'react'
 
 function App() {
+  const [pagina, setPagina] = useState("home")
   return (
     <div>
-      <div className="row">
-        <div className="col">
-          <CardEquipa nome="Sporting CP" cor="Verde" ano={1906} cidade="Lisboa" />
-        </div>   
-        <div className="col">
-          <CardEquipa nome="Porto" cor="Azul" ano={1905} cidade="Porto" />
+      {pagina === "home" && 
+      <div>
+        <div className="row">
+          <div className="col">
+            <CardEquipa nome="Sporting CP" cor="Verde" ano={1906} cidade="Lisboa" />
+          </div>   
+          <div className="col">
+            <CardEquipa nome="Porto" cor="Azul" ano={1905} cidade="Porto" />
+          </div>  
         </div>  
-      </div>  
-      <div className="row">
-        <div className="col">
-          <CardJogador nome="Ronaldo" clube="Real Madrid" idade={40} />
+        <div className="row">
+          <div className="col">
+            <CardJogador nome="Ronaldo" clube="Real Madrid" idade={40} />
+          </div>
+          <div className="col">
+            <CardJogador nome="Messi" clube="Barcelona" idade={39} />
+          </div> 
         </div>
-        <div className="col">
-          <CardJogador nome="Messi" clube="Barcelona" idade={39} />
-        </div> 
-      </div>
-    </div>  
+        <button onClick={() => setPagina("jogadores")}>Ver Jogadores</button>
+      </div>  
+}
+      {pagina === "jogadores" && 
+      <div>Página de jogadores</div>
+    }
+    </div>    
   )
 }
 
