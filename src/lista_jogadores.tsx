dfc6870389e7cdf8cf4b92f2b1248022ef203320
Leonardo <lcat140507@gmail.com>
2026-04-10 22:56:03 +0100

    adicionar pesquisa de jogadores

--- src/lista_jogadores.tsx
+++ src/lista_jogadores.tsx
@@ -1,17 +1,27 @@
 import CardJogador from './card_jogador'
+import { useState } from 'react'
 
 const jogadores = [
   { nome: "Ronaldo", clube: "Real Madrid", idade: 40 },
   { nome: "Messi", clube: "Barcelona", idade: 39 }
 ]
 
+
 function ListaJogadores(){
+    const [jogador, setJogador] = useState("")
     return(
+        
+
         <div>        
-            {jogadores.map((jogador, index) =>(
+            {jogadores.filter(j => j.nome.includes(jogador)).map((jogador, index) =>(
                 <CardJogador key={index} nome={jogador.nome} clube={jogador.clube} idade={jogador.idade}></CardJogador>
             ))}
+
+            <input onChange={e => setJogador(e.target.value)}></input>
+
         </div>
+
+        
     )
 }
 
